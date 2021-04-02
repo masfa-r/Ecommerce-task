392477ace0237e53a65fd42e0dd1400385f15cd7
masfa-r <masfa.rafi99@gmail.com>
2021-04-02 02:01:45 -0700

    first task

---
+++ app.js
@@ -0,0 +1,9 @@
+function myFunc(){
+    var x = document.querySelector("#menu");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display="none";
+    }
+
+ }
